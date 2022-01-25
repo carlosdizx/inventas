44b5323fa4f92f7fdd62b5aacbc453d9698e484e
Carlos Ernesto Diaz <cediazb@siesa.com>
2022-01-25 18:14:42 -0500

    Creada la tabla de prueba, pero no la generica o parametrizada

---
+++ src/components/general/Listado.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-container>
+    <v-card color="indigo" elevation="20">
+      <v-card-title>Soy una lista</v-card-title>
+      <v-card>
+        <!--
+        Código que importa
+        -->
+        <v-data-table
+          :headers="columnas"
+          :items="filas"
+          item-key="documento"
+          class="elevation-1"
+          :search="search"
+          :custom-filter="filterOnlyCapsText"
+        >
+          <template v-slot:top>
+            <v-text-field
+              v-model="search"
+              label="Buscar por documento"
+              class="mx-4"
+            />
+          </template>
+          <template v-slot:item.detalle="{ item }">
+            <DetallesEgresado :datos="item" />
+          </template>
+        </v-data-table>
+        <!--
+        Código que importa
+        -->
+      </v-card>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Listado",
+  data: () => ({
+    search: "",
+    columnas: [
+      { text: "Documento", value: "documento" },
+      { text: "Nombres", value: "nombres" },
+      { text: "Apellidos", value: "apellidos" },
+      { text: "Celular", value: "celular", sortable: false },
+      { text: "Correo", value: "correo", sortable: false },
+      { text: "Más detalles", value: "detalle" },
+    ],
+    filas: [],
+  }),
+  methods: {
+    filterOnlyCapsText(value, search, item) {
+      return (
+        value != null &&
+        search != null &&
+        typeof value === "string" &&
+        value.toString().toLocaleUpperCase().indexOf(search) !== -1
+      );
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/general.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-card color="orange" dark>
+      <v-card-title>Listado con operaciones crud</v-card-title>
+      <v-card-text>
+        <listado />
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import listado from "../components/general/Listado.vue";
+
+export default {
+  name: "general",
+  components: { listado },
+};
+</script>
+
+<style scoped></style>
