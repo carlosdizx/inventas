e4ad67de3555d55fb61ebb1e465851c7aa41535b
Carlos Ernesto Diaz <cediazb@siesa.com>
2022-01-26 22:50:07 -0500

    Se agregó firebase en el main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,24 @@ import "@babel/polyfill";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFzkqsaf-5MDzXHlO-hOFSUI5fd-4iZLw",
+  authDomain: "inventas-app.firebaseapp.com",
+  projectId: "inventas-app",
+  storageBucket: "inventas-app.appspot.com",
+  messagingSenderId: "533111161621",
+  appId: "1:533111161621:web:33225404310a005e489f1c",
+  measurementId: "G-NK2YTKY68M",
+};
+export const app = initializeApp(firebaseConfig);
+
+export const analytics = getAnalytics(app);
+
 Vue.config.productionTip = false;
 
 new Vue({
