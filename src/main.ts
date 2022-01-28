d3a4064fbfeeb9d789359bd7c759f68c4f8491c4
Carlos Ernesto Diaz <cediazb@siesa.com>
2022-01-27 22:40:32 -0500

    Configurado el back para preparar las consultas y manipular la db desde el sdk

--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,26 @@ import "@babel/polyfill";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-import "firebase/storage";
+import firebase from "firebase/app";
+import database from "firebase/database";
+import storage from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBFzkqsaf-5MDzXHlO-hOFSUI5fd-4iZLw",
   authDomain: "inventas-app.firebaseapp.com",
+  databaseURL: "https://inventas-app-default-rtdb.firebaseio.com",
   projectId: "inventas-app",
   storageBucket: "inventas-app.appspot.com",
   messagingSenderId: "533111161621",
   appId: "1:533111161621:web:33225404310a005e489f1c",
   measurementId: "G-NK2YTKY68M",
 };
-export const app = initializeApp(firebaseConfig);
 
-export const analytics = getAnalytics(app);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const DATABASE = database;
+export const STORAGE = storage;
 
 Vue.config.productionTip = false;
 
