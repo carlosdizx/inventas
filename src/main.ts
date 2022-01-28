cf99492bc3a74d18b806a395d0843f6a3b772c4b
Carlos Ernesto Diaz <cediazb@siesa.com>
2022-01-27 23:07:33 -0500

    Desacoplada la config de firebase, ademas, se ignoró ya que en esos archivos hay unas keys

--- src/main.ts
+++ src/main.ts
@@ -7,27 +7,6 @@ import "@babel/polyfill";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
 
-import firebase from "firebase/app";
-import database from "firebase/database";
-import storage from "firebase/storage";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBFzkqsaf-5MDzXHlO-hOFSUI5fd-4iZLw",
-  authDomain: "inventas-app.firebaseapp.com",
-  databaseURL: "https://inventas-app-default-rtdb.firebaseio.com",
-  projectId: "inventas-app",
-  storageBucket: "inventas-app.appspot.com",
-  messagingSenderId: "533111161621",
-  appId: "1:533111161621:web:33225404310a005e489f1c",
-  measurementId: "G-NK2YTKY68M",
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-export const DATABASE = database;
-export const STORAGE = storage;
-
 Vue.config.productionTip = false;
 
 new Vue({
