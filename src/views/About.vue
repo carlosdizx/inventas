7cfd06d856ff6e14a5b48b3b411b0fce2107c86b
dacho7 <davidtobarst@gmail.com>
2022-01-30 20:26:54 +0200

    Added about info

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <h1>Sobre Nosotros</h1>
+    <h3>Introducción</h3>
+    <h4>
+      Inventas se constituye por un grupo de estudiantes desarrolladores,
+      enfocandose en la idea de construir un software capaz de dar solución a
+      las necesidades tecnológicas para las empresas, enfocándose en los
+      principales problemas tecnológicos y las normas de la region
+    </h4>
+    <h3>Misión</h3>
+    <h4>
+      Inventas es un proyecto ERP formado por un equipo de desarrollo enfocado
+      en la calidad y automatización de procesos empresariales
+    </h4>
+  </v-container>
 </template>
